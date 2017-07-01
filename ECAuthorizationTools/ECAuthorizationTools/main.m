72a5e0d02b12893570484912a328b4e4102f4d84
EchoZuo <zuoqianheng@foxmail.com>
2017-07-01 22:55:50 +0800

    first commit

---
+++ ECAuthorizationTools/ECAuthorizationTools/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ECAuthorizationTools
+//
+//  Created by EchoZuo on 2017/6/20.
+//  Copyright © 2017年 Echo.Z. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
